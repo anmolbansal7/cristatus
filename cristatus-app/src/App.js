e88ee3331fa92ecb0e78a6507b611a819ca604f4
anmolbansal7 <abanmolbansal5@gmail.com>
2023-08-22 14:04:54 +0530

    setup cristatus-app

---
+++ cristatus-app/src/App.js
@@ -0,0 +1,15 @@
+import {Toast} from '@cristatus/components'
+
+function App() {
+  return (
+    <div>
+      <header>
+        <p>
+          <Toast/>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
