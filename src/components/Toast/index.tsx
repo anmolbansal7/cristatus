a9b7e5a459aaac30b1fd6ccdd73de456d25efc66
anmolbansal7 <abanmolbansal5@gmail.com>
2023-08-23 17:11:35 +0530

    packages update

--- src/components/Toast/index.tsx
+++ src/components/Toast/index.tsx
@@ -1,10 +1,9 @@
 import React from "react";
-import styles from './styles.module.css';
 
 const Toast = () => {
   // Your Toast component implementation
   return (
-    <h1 className={styles.toast}> This is a Toast Component </h1>
+    <h1> This is a Toast Component </h1>
   )
 };
 
